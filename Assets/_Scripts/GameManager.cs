b91d1a62cf88ef55181f755cd1055e4d463b5308
Brett <brett.mckenzie3@gmail.com>
2025-03-11 19:56:07 -0700

    Finished Up Settings Menu

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -1,16 +1,26 @@
 using TMPro;
+using UnityEditor;
 using UnityEngine;
 
 public class GameManager : SingletonMonoBehavior<GameManager>
 {
     [SerializeField] private int score = 0;
     [SerializeField] private CoinCounterUI coinCounter;
+    [SerializeField] private InputManager inputManager;
+    [SerializeField] private GameObject settingsMenu;
+
+    private bool isMenuActive;
+
+    public bool IsSettingMenuActive => IsSettingMenuActive;
 
     protected override void Awake()
     {
         base.Awake();
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+        inputManager.OnSettingsMenu.AddListener(ToggleSettingsMenu);
+
+        DisableSettingsMenu();
     }
 
     public void IncreaseScore()
@@ -18,4 +28,38 @@ public class GameManager : SingletonMonoBehavior<GameManager>
         score++;
         coinCounter.UpdateScore(score);
     }
+
+    private void ToggleSettingsMenu()
+    {
+        if (isMenuActive) DisableSettingsMenu();
+        else EnableSettingsMenu();
+    }
+
+    private void EnableSettingsMenu()
+    {
+        Time.timeScale = 0f;
+        settingsMenu.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        isMenuActive = true;
+    }
+
+    public void DisableSettingsMenu()
+    {
+        Time.timeScale = 1f;
+        settingsMenu.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        isMenuActive = false;
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif 
+    }
+
 }
